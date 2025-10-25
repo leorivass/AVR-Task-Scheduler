69b66a48bd516ce6d27ff48ae564bc279cfe94b0
Carlos Rivas <leorivas1805@gmail.com>
2025-10-25 16:57:20 -0400

    Remove CMake cache files to ensure clean compilation

--- build/CMakeFiles/main.helf.dir/src/millis.c.o.d
+++
@@ -1,9 +0,0 @@
-CMakeFiles/main.helf.dir/src/millis.c.o: \
- /home/leonel/oldAtmega/scheduler/src/millis.c \
- /usr/lib/avr/include/avr/io.h /usr/lib/avr/include/avr/sfr_defs.h \
- /usr/lib/avr/include/inttypes.h /usr/lib/gcc/avr/7.3.0/include/stdint.h \
- /usr/lib/avr/include/stdint.h /usr/lib/avr/include/avr/iom2560.h \
- /usr/lib/avr/include/avr/portpins.h /usr/lib/avr/include/avr/common.h \
- /usr/lib/avr/include/avr/version.h /usr/lib/avr/include/avr/fuse.h \
- /usr/lib/avr/include/avr/lock.h /usr/lib/avr/include/avr/interrupt.h \
- /home/leonel/oldAtmega/scheduler/includes/millis.h
